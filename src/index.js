faadcc8ce9fbd4f5bbcc8dc6eb8cddaf3156f9b0
smitgor <smitgor120@gmail.com>
2021-06-05 19:08:36 +0530

    almost solved

--- src/index.js
+++ src/index.js
@@ -6,25 +6,26 @@ const path = require("path");
 var bodyParser = require("body-parser");
 var urlencoderParser = bodyParser.urlencoded({extended:true});
 const {sent}=require("process");
-app.use('/style',express.static('style'))
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/" + "index.html");
+app.use(express.static(path.join(__dirname, '/')));
+
+app.get('/index.html', function(req, res) {
+    res.sendFile(__dirname + "/"+"components"+"/"+ "index.html");
 });
 
 app.get('/tickets.html', function(req, res) {
-    res.sendFile(__dirname + "/" + "tickets.html");
+    res.sendFile(__dirname + "/"+"components"+"/"+"tickets.html");
 });
 
 app.get('/parkInfo.html', function(req, res) {
-    res.sendFile(__dirname + "/" + "parkInfo.html");
+    res.sendFile(__dirname + "/"+"components"+"/" + "parkInfo.html");
 });
 
 app.get('/ridesAndSlides.html', function(req, res) {
-    res.sendFile(__dirname + "/" + "ridesAndSlides.html");
+    res.sendFile(__dirname + "/"+"components"+"/" + "ridesAndSlides.html");
 });
 
 app.get('/contact.html', function(req, res) {
-    res.sendFile(__dirname + "/" + "contact.html");
+    res.sendFile(__dirname + "/"+"components"+"/" + "contact.html");
 });
 var server = app.listen(8081, function() {
     var host = server.address().address;
