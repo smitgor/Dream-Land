57f16bbea36662df56e5f5043a19d62b30e6967b
smitgor <smitgor120@gmail.com>
2021-06-07 00:42:02 +0530

    changed to env.port

--- app.js
+++ app.js
@@ -177,8 +177,6 @@ app.get("/owner", (req, res) => {
         });
     });
 });
-var server = app.listen(8081, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Example app listening at http://%s:%s", host, port);
-});
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(3000);
