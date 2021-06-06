38c9766c971ed545683c8d638dfc8e199498f96c
smitgor <smitgor120@gmail.com>
2021-06-07 00:31:12 +0530

    added database name

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var urlencoderParser = bodyParser.urlencoded({extended:true});
 const {sent}=require("process");
 
 var MongoClient = require('mongodb').MongoClient;  
-var url="mongodb+srv://dbsmit:DBSMIT@cluster0.ff23x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+var url="mongodb+srv://dbsmit:DBSMIT@cluster0.ff23x.mongodb.net/dreamLand?retryWrites=true&w=majority";
 
 
 
